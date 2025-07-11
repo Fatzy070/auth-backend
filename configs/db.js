125b22f715577f32399db290ed2a36fc58562743
Faruk Ogunsola <ogunsolafaruk8@example.com>
2025-07-11 10:51:44 +0100

    mongo_atlas

--- configs/db.js
+++ configs/db.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
-
+require('dotenv').config()
 
 const ConnectDB = async()=> {
     try {
-            const db = await mongoose.connect('mongodb://localhost:27017/demo')
+            const db = await mongoose.connect(process.env.MONGO_URL)
             console.log('Connected successfully')
     }catch(error){
         console.error('failed to connect')
--- controller/PageController.js
+++ controller/PageController.js
@@ -89,7 +89,7 @@ const deleteUser = async (req, res) => {
 }
 
 const makeAdmin = async () => {
-  const user = await Auth.findOne({ email: 'cygyxinano@mailinator.com' }) // put your email
+  const user = await Auth.findOne({ email: 'cazitaha@mailinator.com' }) // put your email
   if (user) {
     user.role = 'admin'
     await user.save()
